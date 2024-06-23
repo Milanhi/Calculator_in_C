14e5d59cef76e3978b50268053c47720c05bb185
Milanhi <happymiel12@gmail.com>
2024-06-23 16:20:46 +0200

    Add files via upload

---
+++ Calculator (2).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num1, num2;
+
+    printf("Enter number 1: ");
+    scanf("%d", &num1);
+    printf("Enter number 2: ");
+    scanf("%d", &num2);
+
+
+    
+    printf("Addition: %d\n", (num1 + num2));
+    printf("Subtraction: %d\n", (num1 - num2));
+    printf("multiplication: %d\n", (num1 * num2));
+    printf("division: %d\n", (num1 / num2));
+
+ return 0;
+}
